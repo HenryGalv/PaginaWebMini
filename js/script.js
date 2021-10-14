8e2e424da13e3a5e90a220ea1b8f44b5b1ac8316
HenryGalv <henry100w@hotmail.com>
2021-10-14 13:02:07 -0500

    terminado movil

--- js/script.js
+++ js/script.js
@@ -24,3 +24,28 @@
   // };
 })
   (document);
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $response = d.querySelector("modal-form");
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch("https://formsubmit.co/ajax/henry100w@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target)
+    }).then(
+      res => (res.ok ? res.json() : Promise.reject(res))
+    ).then(json => {
+      console.log(json);
+      location.hash = "#gracias"
+      $form.reset();
+    }).catch(err => {
+      console.log(err);
+      let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+      $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+    }).finally(() => {
+      setTimeout(() => {
+        location.hash = "#cerrar";
+      }, 2000);
+    });
+  })
+})(document);
